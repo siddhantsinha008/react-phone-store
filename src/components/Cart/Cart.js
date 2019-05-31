2c5abfb7d790d2e5ec07cdf91b8fdd2770e172dc
siddhantsinha008 <siddhant.812.sinha@gmail.com>
2019-05-31 13:40:28 +0530

    paypal button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,7 @@ export default class Cart extends Component {
                                     <Title name='your' title='cart'/>
                                     <CartColumn />
                                     <CartList value={value}/>
-                                    <CartTotals value ={value}/>
+                                    <CartTotals value ={value} history={this.props.history}/>
                                 </React.Fragment>
                                 )
                         }
